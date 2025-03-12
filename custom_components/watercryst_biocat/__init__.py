e2112692a7bb19dec27768612e9ebbd53232d55a
route662 <97343669+route662@users.noreply.github.com>
2025-03-12 08:56:12 +0100

    Update __init__.py

--- custom_components/watercryst_biocat/__init__.py
+++ custom_components/watercryst_biocat/__init__.py
@@ -1,7 +1,20 @@
 """Watercryst Biocat Custom Component."""
-DOMAIN = "watercryst_biocat"
+import logging
 
-def setup(hass, config):
-    """Set up the Watercryst Biocat component."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Watercryst Biocat from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
     return True
 
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
