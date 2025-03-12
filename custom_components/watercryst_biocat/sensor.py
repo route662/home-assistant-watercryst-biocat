cef02e07d9204088615f0c1ad3ae8caf0af530bf
route662 <97343669+route662@users.noreply.github.com>
2025-03-12 08:57:51 +0100

    Update sensor.py

--- custom_components/watercryst_biocat/sensor.py
+++ custom_components/watercryst_biocat/sensor.py
@@ -1,55 +1,50 @@
 import logging
 import requests
-import voluptuous as vol
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import TEMP_CELSIUS, PRESSURE_BAR, VOLUME_LITERS
-from homeassistant.helpers.entity import Entity
-import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_API_KEY = "api_key"
-BASE_URL = "https://appapi.watercryst.com/v1/"
+API_URL = "https://appapi.watercryst.com/v1/measurements/direct"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_API_KEY): cv.string,
-})
-
-SENSOR_TYPES = {
-    "waterTemp": ["Public Water Temperature", TEMP_CELSIUS],
-    "pressure": ["Waterpressure to Building", PRESSURE_BAR],
-    "lastWaterTapVolume": ["Last Water Tap Volume", VOLUME_LITERS],
-    "lastWaterTapDuration": ["Last Water Tap Duration", "s"],
-}
-
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the sensor platform."""
-    api_key = config.get(CONF_API_KEY)
-    sensors = []
-
+def get_data(api_key):
+    """Fetch data from the API."""
     try:
-        response = requests.get(BASE_URL + "measurements/direct", headers={"x-api-key": api_key})
+        response = requests.get(API_URL, headers={"x-api-key": api_key})
         response.raise_for_status()
-        data = response.json()
-
-        for key, value in SENSOR_TYPES.items():
-            sensors.append(WatercrystSensor(api_key, key, value[0], value[1]))
-
-        add_entities(sensors, True)
+        return response.json()
     except Exception as e:
-        _LOGGER.error("Failed to fetch data: %s", e)
+        _LOGGER.error("Error fetching data: %s", e)
+        return {}
 
-class WatercrystSensor(Entity):
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up the Watercryst Biocat sensors."""
+    api_key = entry.data["api_key"]
+    data = await hass.async_add_executor_job(get_data, api_key)
+
+    sensors = []
+    for key, name, unit in [
+        ("waterTemp", "Public Water Temperature", "°C"),
+        ("pressure", "Waterpressure to Building", "bar"),
+        ("lastWaterTapVolume", "Last Water Tap Volume", "L"),
+        ("lastWaterTapDuration", "Last Water Tap Duration", "s"),
+    ]:
+        sensors.append(WatercrystSensor(api_key, key, name, unit, data))
+
+    async_add_entities(sensors, True)
+
+class WatercrystSensor(SensorEntity):
     """Representation of a Watercryst sensor."""
 
-    def __init__(self, api_key, sensor_type, name, unit):
+    def __init__(self, api_key, sensor_type, name, unit, data):
         """Initialize the sensor."""
         self._api_key = api_key
         self._sensor_type = sensor_type
         self._name = name
         self._unit = unit
-        self._state = None
+        self._state = data.get(sensor_type)
 
     @property
     def name(self):
@@ -68,11 +63,5 @@ class WatercrystSensor(Entity):
 
     def update(self):
         """Fetch new state data for the sensor."""
-        try:
-            response = requests.get(BASE_URL + "measurements/direct", headers={"x-api-key": self._api_key})
-            response.raise_for_status()
-            data = response.json()
-            self._state = data.get(self._sensor_type)
-        except Exception as e:
-            _LOGGER.error("Error updating sensor: %s", e)
-
+        data = get_data(self._api_key)
+        self._state = data.get(self._sensor_type)
