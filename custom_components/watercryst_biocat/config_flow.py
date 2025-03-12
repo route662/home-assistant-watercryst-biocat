846c584da92165b74a2c29fb599305ab591d1e33
route662 <97343669+route662@users.noreply.github.com>
2025-03-12 09:22:38 +0100

    Update config_flow.py

--- custom_components/watercryst_biocat/config_flow.py
+++ custom_components/watercryst_biocat/config_flow.py
@@ -1,1 +1,18 @@
+"""Config flow for Watercryst Biocat integration."""
+from homeassistant import config_entries
+import voluptuous as vol
+
+from . import DOMAIN
+
+class WatercrystConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Watercryst Biocat."""
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        if user_input is not None:
+            return self.async_create_entry(title="Watercryst Biocat", data=user_input)
+
+        return self.async_show_form(step_id="user", data_schema=vol.Schema({
+            vol.Required("api_key"): str
+        }))
 
