8e525a88766b37d99c7f5aaf23e80eb73fbd8a81
route662 <97343669+route662@users.noreply.github.com>
2025-03-30 18:41:29 +0200

    Bug bei der Abruffrequenz

--- custom_components/watercryst_biocat/__init__.py
+++ custom_components/watercryst_biocat/__init__.py
@@ -8,7 +8,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Watercryst Biocat from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")  # Warten auf die Weiterleitung
+    # Verwende eine Liste für die Plattformen
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
